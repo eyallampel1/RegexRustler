27edc25d4c034fb3e3dd97ce459f4baa084a52fc
Eyal Lampel <eyal.lampel@gmail.com>
2023-12-09 15:18:43 +0200

    split main logic to lib.rs file

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+// lib.rs
+use regex::Regex;
+use std::io::{self, BufRead, BufReader};
+use colored::*;
+use std::fs::File;
+
+pub fn process_file(file_path: &str, regex_pattern: &str) -> io::Result<()> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let regex = Regex::new(regex_pattern).unwrap();
+
+    let mut toggle = false;
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut new_line = String::new();
+        let mut last = 0;
+
+        for mat in regex.find_iter(&line) {
+            new_line.push_str(&line[last..mat.start()]);
+            let matched = if toggle {
+                mat.as_str().red().to_string()
+            } else {
+                mat.as_str().blue().to_string()
+            };
+            toggle = !toggle;
+            new_line.push_str(&matched);
+            last = mat.end();
+        }
+
+        new_line.push_str(&line[last..]);
+        println!("{}", new_line);
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use colored::*;
-use regex::Regex;
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-
+// main.rs
+use std::{env, io};
+use my_regex_project::process_file;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
@@ -11,32 +8,5 @@ fn main() -> io::Result<()> {
         return Ok(());
     }
 
-    let file = File::open(&args[1])?;
-    let reader = BufReader::new(file);
-    let regex = Regex::new(&args[2]).unwrap();
-
-    let mut toggle = false;
-
-    for line in reader.lines() {
-        let line = line?;
-        let mut new_line = String::new();
-        let mut last = 0;
-
-        for mat in regex.find_iter(&line) {
-            new_line.push_str(&line[last..mat.start()]);
-            let matched = if toggle {
-                mat.as_str().red().to_string()
-            } else {
-                mat.as_str().blue().to_string()
-            };
-            toggle = !toggle;
-            new_line.push_str(&matched);
-            last = mat.end();
-        }
-
-        new_line.push_str(&line[last..]);
-        println!("{}", new_line);
-    }
-
-    Ok(())
+    process_file(&args[1], &args[2])
 }
