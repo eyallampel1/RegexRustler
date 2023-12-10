387c0845abf9e1b5079801e54c2c91bb51498bb3
Lampel Eyal <eyel1201@elta.co.il>
2023-12-10 22:03:28 +0200

    Added clap crate support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 // main.rs
 use std::{env, io};
-use my_regex_project::process_file;
+use RegexRustler::process_file;
+use clap::{command, Arg};
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <file_path> <regex_pattern>", args[0]);
-        return Ok(());
-    }
+    let matches= command!().about("Searches(and colors) for a regex pattern in a text file")
+    .arg(
+        Arg::new("file-path")
+        .short('f')
+        .long("path")
+        .aliases(["fpath", "path", "text-file","file"])
+        .value_name("FILE")
+        .required(true)
+        .help("Path to the Text file to search for example: /home/user/text.txt")
+    )   
+    .arg(
+        Arg::new("regex-pattern")
+        .short('r')
+        .long("regex")
+        .value_name("REGEX")
+        .required(true)
+        .help("Regex pattern to match for example: [a-z]")
+    )
+        .get_matches();
 
-    process_file(&args[1], &args[2])
+    process_file(matches.get_one::<String>("file-path").unwrap(), matches.get_one::<String>("regex-pattern").unwrap())
 }
